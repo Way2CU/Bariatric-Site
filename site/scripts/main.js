5fbec34ecf8d8caced65d65f164c6ffba532a3ff
Mladen Mijatov <meaneye.rcf@gmail.com>
2017-06-22 16:02:26 +0200

    Import from real site.

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -48,9 +48,49 @@ Site.is_mobile = function() {
 /**
  * Function called when document and images have been completely loaded.
  */
+
+ function dialog() {
+
+	// calling the dialog.
+	var video_dialog = new Dialog();
+
+	//Modefy the dialog.
+
+	video_dialog.setTitle(language_handler.getText(null, 'dialog_video_title'));
+	video_dialog.setSize(640, 330);
+
+	$('a.youtube').not('.mobile').click(function(event) {
+
+		// prevent link from working.
+		event.preventDefault();
+
+		// set content from URL and show it.
+		video_dialog.setContentFromURL($(this).attr('href'));
+		video_dialog.showWhenReady();
+	});
+}
+
 Site.on_load = function() {
-	if (Site.is_mobile())
-		Site.mobile_menu = new Caracal.MobileMenu();
+
+	if (!Site.is_mobile()){
+	dialog();
+	}
+	
+	$('form').on('dialog-show', function() {
+	$('form').hide();
+	$('div.send').hide();
+	var thankyou = "/thankyou" + window.location.search;
+	// handle analytics event
+	$('form').on('analytics-event', function(event, data) {
+		if (!data.error)
+			dataLayer.push({
+            	'event':'leadSent'
+            });
+		window.location.replace(thankyou);
+	});
+	return false;
+});
+	
 };
 
 
